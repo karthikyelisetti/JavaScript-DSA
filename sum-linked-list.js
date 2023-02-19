ac3b758c1a865c3b88ab0f4d1e9e0836d5bf3b0f
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-02-19 09:24:03 +0530

    updating the sum of linked lists

--- sum-linked-list.js
+++ sum-linked-list.js
@@ -5,22 +5,62 @@ class Node {
   }
 }
 
-function newNode(value) {
-  let temp = new Node(value);
-  return temp;
+// function newNode(value) {
+//   let temp = new Node(value);
+//   return temp;
+// }
+
+class LinkedList {
+  constructor(value) {
+    this.head = { value: value, next: null };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  print = () => {
+    let arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return arr.join(" -> ");
+  };
+
+  append = (value) => {
+    const newNode = new Node(value);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+    return this;
+  };
 }
 
-function reverseArray(arr) {
+function reverseArray(arr, order) {
   var revArr = [];
-  for (var i = arr.length - 1; i >= 0; i--) {
-    revArr.push(arr[i]);
+  if (order == "reverse") {
+    for (var i = arr.length - 1; i >= 0; i--) {
+      revArr.push(arr[i]);
+    }
+  } else {
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+      revArr.push(arr[i]);
+    }
   }
   return revArr.join("");
 }
 
 function addLinkedList(l1, l2, order) {
-  var l1Arr = [], l2Arr = [];
-  var num1 = 0, num2 = 0, sum = 0;
+  var l1Arr = [],
+    l2Arr = [];
+  var num1 = 0,
+    num2 = 0,
+    sum = 0;
   //Pushing the linked list to array
   while (l1 != null) {
     l1Arr.push(l1.value);
@@ -31,39 +71,41 @@ function addLinkedList(l1, l2, order) {
     l2 = l2.next;
   }
 
-  if (order == 'reverse') {
-    num1 = parseInt(reverseArray(l1Arr));
-    num2 = parseInt(reverseArray(l2Arr))
-    sum = num1 + num2
-    var list = sum.toString().split("");
-    var lists;
-    for (i = list.length-1; i >= 0; i--) {
-        console.log(list[i])
-        if (lists == null) {
-            lists = newNode(list[i]);
-            console.log(lists);
-        } else {
-            lists.next = newNode(list[i]);
-            console.log(lists);
-        }
-    }
-    console.log(lists);
-  } else if (order == 'forward'){
-        
-  }
-  
+  num1 = parseInt(reverseArray(l1Arr, order));
+  num2 = parseInt(reverseArray(l2Arr, order));
+  sum = num1 + num2;
+  var list = sum.toString().split("");
+  var lists;
 
-  return console.log('The sum of two linked lists is: ' + sum);
+  if (order == "reverse") {
+    for (i = list.length - 1; i >= 0; i--) {
+      if (lists == null) {
+        lists = new LinkedList(list[i]);
+      } else {
+        lists.append(list[i]);
+      }
+    }
+  } else if (order == "forward") {
+    for (i = 0; i < list.length; i++) {
+      if (lists == null) {
+        lists = new LinkedList(list[i]);
+      } else {
+        lists.append(list[i]);
+      }
+    }
+  }
+  console.log(lists.print());
+  console.log("The sum of two linked lists is: " + sum);
 }
 
 // Creating a linked list-1
-let list1 = newNode(7);
+let list1 = newNode(6);
 list1.next = newNode(1);
-list1.next.next = newNode(6);
+list1.next.next = newNode(7);
 
 // Creating a linked list-2
-let list2 = newNode(5);
+let list2 = newNode(2);
 list2.next = newNode(9);
-list2.next.next = newNode(2);
+list2.next.next = newNode(5);
 
-addLinkedList(list1, list2, 'reverse');
+addLinkedList(list1, list2, "forward");
